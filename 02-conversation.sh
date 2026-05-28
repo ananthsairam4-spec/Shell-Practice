73f27555b7816d7e5f6036b9fc55b15ba1718511
Ganjam Anantha Sairam <ananthsairam5@gmail.com>
2026-05-28 22:26:11 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Trump: Hey Iran, you better not mess with us!"
-echo "Iran: Oh yeah? What are you gonna do about it?"
+echo "Trump: Hey Iran you better not mess with us"
+echo "Iran: Oh yeah What are you gonna do about it"
 echo "Trump: Ill send my best troops to the Middle East and make sure you regret it"
-echo "Iran: Bring it on, we have the best troops in the world!"
+echo "Iran: Bring it on we have the best troops in the world"
