5fa26648e403d60f7054d3e21e4b7ca72fb7c4e4
Ganjam Anantha Sairam <ananthsairam5@gmail.com>
2026-05-28 22:23:57 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,5 +2,5 @@
 
 echo "Trump: Hey Iran, you better not mess with us!"
 echo "Iran: Oh yeah? What are you gonna do about it?"
-echo "Trump: I'll send my best troops to the Middle East and make sure you regret it
+echo "Trump: Ill send my best troops to the Middle East and make sure you regret it"
 echo "Iran: Bring it on, we have the best troops in the world!"
