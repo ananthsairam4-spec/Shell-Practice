9db7e05d6ab266d38cf6c41167c81de0628874ef
Ganjam Anantha Sairam <ananthsairam5@gmail.com>
2026-05-28 22:48:52 +0530

    hello world

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PERSON1=Trump
-PERSON2=Iran
-echo "Trump: Hey Iran you better not mess with us"
-echo "Iran: Oh yeah What are you gonna do about it"
-echo "Trump: Ill send my best troops to the Middle East and make sure you regret it"
-echo "Iran: Bring it on we have the best troops in the world"
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}: Hey ${PERSON2} you better not mess with us"
+echo "${PERSON2}: Oh yeah What are you gonna do about it"
+echo "${PERSON1}: Ill send my best troops to the Middle East and make sure you regret it"
+echo "${PERSON2}: Bring it on we have the best troops in the world"
