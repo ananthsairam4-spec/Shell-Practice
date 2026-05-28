bbb2a2d4f21cf4775b2fd2541cb2da475a1e2dc8
Ganjam Anantha Sairam <ananthsairam5@gmail.com>
2026-05-28 22:44:52 +0530

    hello world

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Iran
+echo "Trump: Hey Iran you better not mess with us"
+echo "Iran: Oh yeah What are you gonna do about it"
+echo "Trump: Ill send my best troops to the Middle East and make sure you regret it"
+echo "Iran: Bring it on we have the best troops in the world"
