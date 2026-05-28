93d24c2d7897e725fcc80570501565b574c3aee0
Ganjam Anantha Sairam <ananthsairam5@gmail.com>
2026-05-28 22:59:14 +0530

    hello world

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,3 +4,9 @@ echo "Please enter your username::"
 read USER_NAME #USER_NAME is variable, whatever you type in will be stored in this variable
 
 echo "Username is $USER_NAME"
+
+echo "Please enter your password::"
+read -s PASSWORD
+
+echo "Password is $PASSWORD"
+
